d0b13bfcbd7348fbce07b1e0d467815f58b585f8
Aaron Bruckner <aaronbruckner@gmail.com>
2018-02-05 22:19:51 -0800

    Updated problem versions.

--- problemsByType/src/binarySearchTree/p001isBST.js
+++ problemsByType/src/binarySearchTree/p001isBST.js
@@ -4,7 +4,8 @@
 * Given the root node, this function determines if the resulting tree upholds the BST property
 * (all nodes to the left are less than or equal to, all nodes to right are greater than or equal to).
 *
-* @param {Node} root - 
+* @param {Node} root - the root node of the tree to check.
+* @return {boolean} - true if the tree adheres to the BST property, false otherwise.
 */
 function isBST(root) {
   if (!root) {
