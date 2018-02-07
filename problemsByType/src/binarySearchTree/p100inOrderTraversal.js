1d3d792bbdafe9bc8aca95fde89db5a8dedea6e5
Aaron Bruckner <aaronbruckner@gmail.com>
2018-02-06 20:18:55 -0800

    Completed inorder BST traversal problem.

--- problemsByType/src/binarySearchTree/p100inOrderTraversal.js
+++ problemsByType/src/binarySearchTree/p100inOrderTraversal.js
@@ -1,1 +1,15 @@
+/**
+* Traverses tree inorder and returns values.
+*/
+function inorderTraversal(node, order = []) {
+  if (!node) {
+    return order;
+  }
+  inorderTraversal(node.left, order);
+  order.push(node.value);
+  inorderTraversal(node.right, order)
 
+  return order;
+}
+
+module.exports = inorderTraversal;
