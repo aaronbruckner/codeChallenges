cd2920715fb397e2fc89308a39a4825a391039d6
Aaron Bruckner <aaronbruckner@gmail.com>
2018-02-08 21:06:55 -0800

    Start of LCA

---
+++ problemsByType/src/binarySearchTree/p005lowestCommonAncestor.js
@@ -0,0 +1,9 @@
+/**
+* Specialized lowest common ancestor solution for BSTs. Trees must have unique values.
+* Runs in O(h) where h is tree height. This is very similar to a modified binary search.
+*/
+function lowestCommonAncestor(nodeA, nodeB, root) {
+
+}
+
+module.exports = lowestCommonAncestor;
---
+++ problemsByType/test/binarySearchTree/p005lowestCommonAncestorSpec.js
@@ -0,0 +1,11 @@
+const mocha = require('mocha');
+const describe = mocha.describe;
+const it = mocha.it;
+const assert = require('chai').assert;
+
+const Node = require('../../src/binarySearchTree/Node.js');
+const lowestCommonAncestor = require('../../src/binarySearchTree/p005lowestCommonAncestor.js');
+
+describe('lowestCommonAncestor', () => {
+
+});
