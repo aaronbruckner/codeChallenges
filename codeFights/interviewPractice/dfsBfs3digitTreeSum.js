256dd8f8e740022fbb98ab9a89b9531503d8a508
Aaron Bruckner <aaronbruckner@gmail.com>
2018-02-22 23:08:58 -0800

    Interview prep DFS BFS p3

---
+++ codeFights/interviewPractice/dfsBfs3digitTreeSum.js
@@ -0,0 +1,96 @@
+/*
+We're going to store numbers in a tree. Each node in this tree will store a single digit (from 0 to 9), and each path from root to leaf encodes a non-negative integer.
+
+Given a binary tree t, find the sum of all the numbers encoded in it.
+
+Example
+
+For
+t = {
+    "value": 1,
+    "left": {
+        "value": 0,
+        "left": {
+            "value": 3,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 1,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 4,
+        "left": null,
+        "right": null
+    }
+}
+the output should be
+digitTreeSum(t) = 218.
+There are 3 numbers encoded in this tree:
+
+Path 1->0->3 encodes 103
+Path 1->0->1 encodes 101
+Path 1->4 encodes 14
+and their sum is 103 + 101 + 14 = 218.
+t = {
+    "value": 0,
+    "left": {
+        "value": 9,
+        "left": null,
+        "right": null
+    },
+    "right": {
+        "value": 9,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 3,
+            "left": null,
+            "right": null
+        }
+    }
+}
+the output should be
+digitTreeSum(t) = 193.
+Because 09 + 091 + 093 = 193
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] tree.integer t
+
+A tree of integers. It's guaranteed that the sum of encoded integers won't exceed 252.
+
+Guaranteed constraints:
+1 ≤ tree size ≤ 2000,
+1 ≤ tree depth ≤ 9,
+0 ≤ node value ≤ 9.
+
+[output] integer64
+
+The sum of the integers encoded in t, as described above.
+*/
+//
+// Definition for binary tree:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+function digitTreeSum(node, number = '') {
+    if (!node) {
+        return 0;
+    }
+    number += node.value;
+    if (!node.left && !node.right) {
+        return parseInt(number);
+    }
+    return digitTreeSum(node.left, number) + digitTreeSum(node.right, number);
+}
