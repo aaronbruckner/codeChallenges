5a2236e3ba4bc68a9e8a0de861a2385d47a47df4
Aaron Bruckner <aaronbruckner@gmail.com>
2019-04-22 18:24:07 -0700

    p021isIPv4Address.ts

---
+++ codeFights/arcade/intro/p021isIPv4Address.ts
@@ -0,0 +1,61 @@
+/*
+An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+
+Given a string, find out if it satisfies the IPv4 address naming rules.
+
+Example
+
+For inputString = "172.16.254.1", the output should be
+isIPv4Address(inputString) = true;
+
+For inputString = "172.316.254.1", the output should be
+isIPv4Address(inputString) = false.
+
+316 is not in range [0, 255].
+
+For inputString = ".254.255.0", the output should be
+isIPv4Address(inputString) = false.
+
+There is no first number.
+
+Input/Output
+
+[execution time limit] 5 seconds (ts)
+
+[input] string inputString
+
+A string consisting of digits, full stops and lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 30.
+
+[output] boolean
+
+true if inputString satisfies the IPv4 address naming rules, false otherwise.
+*/
+
+const ipPartRegEx = new RegExp('^(0|([1-9][0-9]{0,2}))$');
+
+function isIPv4Address(inputString: string): boolean {
+    const ipParts: string[] = inputString.split('.');
+    if (ipParts.length !== 4) {
+        return false;
+    }
+    
+    for (let part of ipParts) {
+        if (part.length > 3) {
+            return false;
+        }
+        
+        if (!ipPartRegEx.test(part)) {
+            return false;
+        }
+        
+        if (parseInt(part) > 255) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
