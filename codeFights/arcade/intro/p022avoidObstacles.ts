04fdbcfc157e25b8bb893892052cc2eb91cd604c
Aaron Bruckner <aaronbruckner@gmail.com>
2019-05-05 16:55:56 -0700

    p022 avoidObstacles

---
+++ codeFights/arcade/intro/p022avoidObstacles.ts
@@ -0,0 +1,48 @@
+/*
+You are given an array of integers representing coordinates of obstacles situated on a straight line.
+
+Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+
+Find the minimal length of the jump enough to avoid all the obstacles.
+
+Example
+
+For inputArray = [5, 3, 6, 7, 9], the output should be
+avoidObstacles(inputArray) = 4.
+
+Check out the image below for better understanding:
+
+
+
+Input/Output
+
+[execution time limit] 5 seconds (ts)
+
+[input] array.integer inputArray
+
+Non-empty array of positive integers.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 1000,
+1 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The desired length.
+ */
+function avoidObstacles(inputArray: number[]): number {
+    let candidate = 1;
+    while (true) {
+        let candidateFound = true;
+        for (let input of inputArray) {
+            if (input % candidate === 0) {
+                candidateFound = false;
+                break;
+            }
+        }
+        if (candidateFound) {
+            return candidate;
+        }
+        candidate++;
+    }
+}
