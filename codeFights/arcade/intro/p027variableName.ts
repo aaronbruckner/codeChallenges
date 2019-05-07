01206f7281fbff299456cb1b1e7f835d8e1d1c6a
Aaron Bruckner <aaronbruckner@gmail.com>
2019-05-06 21:09:04 -0700

    p027 variableName

---
+++ codeFights/arcade/intro/p027variableName.ts
@@ -0,0 +1,31 @@
+/*
+Correct variable names consist only of English letters, digits and underscores and they can't start with a digit.
+
+Check if the given string is a correct variable name.
+
+Example
+
+For name = "var_1__Int", the output should be
+variableName(name) = true;
+For name = "qq-q", the output should be
+variableName(name) = false;
+For name = "2w2", the output should be
+variableName(name) = false.
+Input/Output
+
+[execution time limit] 5 seconds (ts)
+
+[input] string name
+
+Guaranteed constraints:
+1 ≤ name.length ≤ 10.
+
+[output] boolean
+
+true if name is a correct variable name, false otherwise.
+*/
+const isValidVariableRegExp = new RegExp('^[a-zA-Z_][a-zA-Z0-9_]*$');
+function variableName(name: string): boolean {
+    return isValidVariableRegExp.test(name);
+}
+
