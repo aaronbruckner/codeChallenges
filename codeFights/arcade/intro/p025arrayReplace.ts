b5351e7fb9fddcf78c7bf964690743cabb86fb6e
Aaron Bruckner <aaronbruckner@gmail.com>
2019-05-06 20:58:57 -0700

    p025 arrayReplace

---
+++ codeFights/arcade/intro/p025arrayReplace.ts
@@ -0,0 +1,39 @@
+/*
+Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+
+Example
+
+For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
+arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
+
+Input/Output
+
+[execution time limit] 5 seconds (ts)
+
+[input] array.integer inputArray
+
+Guaranteed constraints:
+0 ≤ inputArray.length ≤ 104,
+0 ≤ inputArray[i] ≤ 109.
+
+[input] integer elemToReplace
+
+Guaranteed constraints:
+0 ≤ elemToReplace ≤ 109.
+
+[input] integer substitutionElem
+
+Guaranteed constraints:
+0 ≤ substitutionElem ≤ 109.
+
+[output] array.integer
+*/
+function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] === elemToReplace) {
+            inputArray[i] = substitutionElem;
+        }
+    }
+    return inputArray;
+}
+
